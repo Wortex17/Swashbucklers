6ed7004507394488885c1d4aee25739f5d85df2b
Worry Darque <worry.darque@gmail.com>
2015-05-22 11:55:29 +0200

    Retain history in gh-pages to allow custom modifications

--- deploy-ghpages.sh
+++ deploy-ghpages.sh
@@ -3,15 +3,17 @@ set -e # exit with nonzero exit code if anything fails
 
 # clear and re-create the _deploy directory
 rm -rf _deploy || exit 0;
-mkdir _deploy;
-mkdir _deploy/book/
+mkdir _deploy
 
-# move required files to the _deploy diretcory
+# go to the _deploy directory and pull the Git repo with gh-pages
+cd _deploy
+git checkout --branch gh-pages --depth 1
+
+# replace the book with the freshly built one
+rm -rf _deploy/book || exit 0;
+mkdir _deploy/book
 mv Letterpress/_html/* _deploy/book/
 
-# go to the _deploy directory and create a *new* Git repo
-cd _deploy
-git init
 
 # inside this git repo we'll pretend to be a new user
 git config user.name "Travis CI"
@@ -26,4 +28,4 @@ git commit -m "Deploy to GitHub Pages"
 # repo's gh-pages branch. (All previous history on the gh-pages branch
 # will be lost, since we are overwriting it.) We redirect any output to
 # /dev/null to hide any sensitive credential data that might otherwise be exposed.
-git push --force --quiet "https://${GH_TOKEN}@${GH_REF}" master:gh-pages > /dev/null 2>&1
+git push --quiet "https://${GH_TOKEN}@${GH_REF}" master:gh-pages > /dev/null 2>&1
