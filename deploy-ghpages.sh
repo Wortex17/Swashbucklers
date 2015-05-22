3924bf826c79bfcdac2f7ae8ab75f0b2436e707c
Worry Darque <worry.darque@gmail.com>
2015-05-22 15:21:34 +0200

    Trying if-else clause to capture empty commits

--- deploy-ghpages.sh
+++ deploy-ghpages.sh
@@ -20,13 +20,24 @@ cd _deploy
 git config user.name "Travis CI"
 git config user.email "travis@nucular-bacon.com"
 
-# The first and only commit to this new Git repo contains all the
-# files present with the commit message "Deploy to GitHub Pages".
-git add .
-git commit -m "Deploy to GitHub Pages"
+#count the changes that have been made
+nChanges = $(git diff --numstat | wc -l)
 
-# Force push from the current repo's master branch to the remote
-# repo's gh-pages branch. (All previous history on the gh-pages branch
-# will be lost, since we are overwriting it.) We redirect any output to
-# /dev/null to hide any sensitive credential data that might otherwise be exposed.
-git push --quiet "https://${GH_TOKEN}@${GH_REF}" master:gh-pages > /dev/null 2>&1
+if [[ $nChanges > 0 ]] ;then
+
+    # The first and only commit to this new Git repo contains all the
+    # files present with the commit message "Deploy to GitHub Pages".
+    git add .
+    git commit -m "Deploy to GitHub Pages"
+
+    # Force push from the current repo's master branch to the remote
+    # repo's gh-pages branch. (All previous history on the gh-pages branch
+    # will be lost, since we are overwriting it.) We redirect any output to
+    # /dev/null to hide any sensitive credential data that might otherwise be exposed.
+    git push --quiet "https://${GH_TOKEN}@${GH_REF}" master:gh-pages > /dev/null 2>&1
+
+else
+
+    echo "No changes to commit"
+
+fi
