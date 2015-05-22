362cd6c9819dcfdcf440eaf1353517ea763fece7
Worry Darque <worry.darque@gmail.com>
2015-05-22 14:53:06 +0200

    step into _deploy dir later

--- deploy-ghpages.sh
+++ deploy-ghpages.sh
@@ -5,15 +5,13 @@ set -e # exit with nonzero exit code if anything fails
 rm -rf _deploy || exit 0;
 mkdir _deploy
 
-# go to the _deploy directory and pull the Git repo with gh-pages
+# pull the Git repo with gh-pages to the _deploy directory
 git clone "https://${GH_REF}" --branch gh-pages --depth 1 _deploy/
-cd _deploy
 
 # replace the book with the freshly built one
 rm -rf _deploy/book || exit 0;
 mkdir _deploy/book
 mv Letterpress/_html/* _deploy/book/
-
 
 # inside this git repo we'll pretend to be a new user
 git config user.name "Travis CI"
@@ -21,6 +19,7 @@ git config user.email "travis@nucular-bacon.com"
 
 # The first and only commit to this new Git repo contains all the
 # files present with the commit message "Deploy to GitHub Pages".
+cd _deploy
 git add .
 git commit -m "Deploy to GitHub Pages"
 
