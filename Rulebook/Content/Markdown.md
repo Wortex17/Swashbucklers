3f35cdf803242cbed87b927b9fb208ec5e5562c2
Worry Darque <worry.darque@gmail.com>
2015-06-04 18:28:34 +0200

    Add loremipsum and some custom css

--- Rulebook/Content/Markdown.md
+++ Rulebook/Content/Markdown.md
@@ -4,6 +4,28 @@ For now this will be a playground
 
 github.com
 
+## Title level 2
+
+{{ lorem_ipsum256 }}
+
+### Title level 3
+
+{{ lorem_ipsum256 }}
+
+#### Title level 4
+
+{{ lorem_ipsum256 }}
+
+##### Title level 5
+
+{{ lorem_ipsum256 }}
+
+###### Title level 6
+
+{{ lorem_ipsum256 }}
+
+- - -
+
 {{current_language}} ({{current_language_en}})
 
 {{> Markdown/basics}}
--- Rulebook/Definitions/basic_en.js
+++ Rulebook/Definitions/basic_en.js
@@ -1,4 +1,16 @@
+var fs = require('fs');
+var path = require('path');
+
+var loremipsum = fs.readFileSync(path.join(__dirname, './loremipsum.txt')).toString();
+
 module.exports = {
     "current_language": "english",
-    "current_language_en": "english"
+    "current_language_en": "english",
+
+    "lorem_ipsum": loremipsum.trim(),
+    "lorem_ipsum16": loremipsum.slice(0, 16).trim(),
+    "lorem_ipsum32": loremipsum.slice(0, 32).trim(),
+    "lorem_ipsum64": loremipsum.slice(0, 64).trim(),
+    "lorem_ipsum128": loremipsum.slice(0, 128).trim(),
+    "lorem_ipsum256": loremipsum.slice(0, 256).trim()
 };
